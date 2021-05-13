0aee8d53e02660715b3744716dfe0ad4628f7521
Madhelem <Madhelem.0509@gmail.com>
2021-05-13 00:49:07 -0400

    PREGUNTA 1

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+/*PREGUNTA 1:
+Estamos creando una aplicación de apuestas de fútbol.
+Supongamos que obtenemos datos de un servicio web sobre un determinado juego (vera un ejemplo del objeto “game” al final de este enunciado) trabajara con esos datos.
+Sus tareas: */
+const game = {
+      team1: 'Bayern Munich',
+      team2: 'Borrussia Dortmund',
+      players: [
+      ['Neuer','Pavard','Martinez','Alaba','Davies','Kimmich','Goretzka','Coman','Muller','Gnarby','Lewandowski',],
+      [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+      ],
+      ],
+      score: '4:0',
+      scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+      date: 'Nov 9th, 2037',
+      odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+      },
+      };
+      
+      
